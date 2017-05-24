20f216ece8e43d33a6a7b49c72a9ce98099ecff1
Pohua Lee <pohualee@Pohuas-Mac-mini.local>
2017-05-24 00:42:49 -0400

    Added css, js, and fixing up the index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="text-center">Concord Wireless Repairs</h1>
+
+      <div id="" class="text-center">
+        <form class="" action="#" method="post">
+          <input type="text" name="search-input" value="" placeholder="Search Repair">
+          <button type="button" name="search-button">Search Repair</button>
+        </form>
+      </div><!-- End of Search DIV -->
+
+      <div class="">
+        <table class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Date Added</th>
+              <th>Date Updated</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+    </table>
+      </div><!-- End of Repair Table DIV -->
+
+      <footer class="footer">
+        <h6 class="text-center">Concord Wireless Repairs</h6>
+      </footer>
+    </div><!-- End of Container DIV -->
+  </body>
+</html>
